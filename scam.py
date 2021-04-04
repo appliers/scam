e4f0067d3d4df239cad8ffc84cbeb5d44315419b
xkwm1 <48575811+xkwm1@users.noreply.github.com>
2021-04-04 16:58:09 -0700

    Update scam.py

--- scam.py
+++ scam.py
@@ -16,7 +16,7 @@ names = 'jason'
 for name in names:
 	name_extra = ''.join(random.choice(string.digits))
 
-	username = name.lower() + name_extra + '@yahoo.com'
+	username = 'jason' + name_extra + '@yahoo.com'
 	password = ''.join(random.choice(chars) for i in range(8))
 
 	requests.post(url, allow_redirects=False, data={
